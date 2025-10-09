4d3c30f6753e0e8da0643ac61b8af40806723fe9
Ram.Rv <ram.rv@flender.com>
2025-10-10 00:20:27 +0530

    RoundRoller(Frontend) : get and post for roll dice provided

--- RoundRoller-Frontend/src/app/core/services/participant-api.service.ts
+++ RoundRoller-Frontend/src/app/core/services/participant-api.service.ts
@@ -3,6 +3,7 @@ import {catchError, Observable, throwError} from 'rxjs';
 import {ParticipantResponse} from '../models/participant-response.model';
 import {HttpClient} from '@angular/common/http';
 import {ParticipantRequest} from '../models/participant-request.model';
+import {RollResponse} from '../models/roll-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,25 @@ export class ParticipantApiService {
   addParticipants(participant: ParticipantRequest) : Observable<ParticipantResponse>{
       const url = '/api/participants/addParticipants'
       return this.http.post<ParticipantResponse>(url,participant).pipe(catchError(error => {
+        console.log(`Error while during the response ${error}`);
         return throwError(() => error);
       }))
   }
+
+  getParticipants(): Observable<ParticipantResponse>{
+      const url = '/api/participants/getAllParticipants'
+      return this.http.get<ParticipantResponse>(url).pipe((catchError(error =>{
+        console.log(`Error while during the response ${error}`);
+        return throwError(() => error);
+      })))
+  }
+
+    rollDice(): Observable<RollResponse>{
+      const url = '/api/participants/rollDice'
+      return this.http.post<RollResponse>(url,{}).pipe((catchError(error => {
+        console.log(`Error while during the response ${error}`);
+        return throwError(() => error);
+      })))
+    }
+
 }
