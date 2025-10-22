0ba03924604f2077c45e55dc2b5b505ed7d1a558
Ram.Rv <ram.rv@flender.com>
2025-10-22 20:45:55 +0530

    RoundRoller(Frontend) : routing provided

--- RoundRoller-Frontend/src/app/app.component.ts
+++ RoundRoller-Frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {ParticipantEntryComponent} from './features/participant-entry/participant-entry.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ParticipantEntryComponent],
+  imports: [ParticipantEntryComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
