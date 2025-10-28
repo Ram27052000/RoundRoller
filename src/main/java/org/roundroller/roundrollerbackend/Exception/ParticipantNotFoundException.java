0809dbfcf625cc2813345df88455691138ba2c83
Ram.Rv <ram.rv@flender.com>
2025-10-28 20:38:52 +0530

    RoundRoller(backend): removed folder frontend

--- src/main/java/org/roundroller/roundrollerbackend/Exception/ParticipantNotFoundException.java
+++ src/main/java/org/roundroller/roundrollerbackend/Exception/ParticipantNotFoundException.java
@@ -3,8 +3,6 @@ package org.roundroller.roundrollerbackend.Exception;
 
 public class ParticipantNotFoundException extends RuntimeException {
 
-    public ParticipantNotFoundException() {
-    }
     public ParticipantNotFoundException(String message) {
         super(message);
     }
